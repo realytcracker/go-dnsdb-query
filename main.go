a4bf40140177c6acc9692b6ac3be125cc1b37544
ytcracker <ytcracker@ytcracker.com>
2018-09-24 17:10:54 -0700

    fixed strconv thing and added dedupe

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -42,6 +43,7 @@ type DNSRecord []struct {
 func main() {
 	var config Config
 	var dnsRecord DNSRecord
+	var raw, deduped []string
 
 	//get 24h ago epoch time
 	now := time.Now()
@@ -69,22 +71,50 @@ func main() {
 
 	client := &http.Client{Transport: tr}
 
-	req, err := http.NewRequest("GET", config.DNSDBURL+"/lookup/rrset/name/"+domain+"/a/?limit=999999&time_last_after="+string(secs), nil)
+	req, err := http.NewRequest("GET", config.DNSDBURL+"/lookup/rrset/name/"+domain+"/a/?limit=999999&time_last_after="+strconv.FormatInt(secs, 10), nil)
 	req.Header.Add("X-API-Key", config.DNSDBAPIKey)
 	req.Header.Add("Accept", "application/json")
+
+	//fmt.Println("fetching " + config.DNSDBURL + "/lookup/rrset/name/" + domain + "/a/?limit=999999&time_last_after=" + strconv.FormatInt(secs, 10) + "...")
 
 	response, _ := client.Do(req)
 	body, _ := ioutil.ReadAll(response.Body)
 	response.Body.Close()
 
 	//the json you get from DNSDB is bad, so format it correctly
+	//fmt.Println("reformatting json...")
+
 	validjson := "[" + strings.Replace(string(body), "}", "},", -1)
 	validjson = validjson[:len(validjson)-2] + "]"
 
+	//fmt.Println("decoding json...")
 	_ = json.NewDecoder(strings.NewReader(validjson)).Decode(&dnsRecord)
 
 	for _, record := range dnsRecord {
 		//remove period at the end
-		fmt.Println(record.RRName[:len(record.RRName)-1])
+		raw = append(raw, record.RRName[:len(record.RRName)-1])
 	}
+	//fmt.Println("removing duplicates...")
+	deduped = removeDuplicates(raw)
+
+	for _, record := range deduped {
+		//remove period at the end
+		fmt.Println(record)
+	}
+}
+
+func removeDuplicates(elements []string) []string {
+	encountered := map[string]bool{}
+
+	// Create a map of all unique elements.
+	for v := range elements {
+		encountered[elements[v]] = true
+	}
+
+	// Place all keys from the map into a slice.
+	result := []string{}
+	for key, _ := range encountered {
+		result = append(result, key)
+	}
+	return result
 }
